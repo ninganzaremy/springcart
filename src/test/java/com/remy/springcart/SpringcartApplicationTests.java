61c5ba124a4542cf0df718c8399c02474f36c2c3
Remy Ninganza <remy@ninganzaremy.com>
2024-02-16 23:13:09 -0700

    spring boot initial setup

---
+++ src/test/java/com/remy/springcart/SpringcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remy.springcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcartApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
